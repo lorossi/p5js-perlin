d5e8d69d296b6e55ba6016808cf661a0e3784ff7
lorossi <lo.rossi@live.com>
2020-12-06 13:40:21 +0100

    added link to my website in console

--- js/main.js
+++ js/main.js
@@ -1,4 +1,5 @@
 $(document).ready(function() {
+  console.log("%cSnooping around? Check the repo! https://github.com/lorossi/painting-bubbles", "color:white;font-size:1.5rem;");
   $(document).on("click keydown", function() {
     $("#instructions").fadeOut(3000);
   });
